251069c8cdd3bcb97db7c8a1820a25348c9211bf
Wargoth-ks <warheart1986@gmail.com>
2024-06-06 11:06:14 +0300

    Status server

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 // import bootstrap from 'bootstrap'
 
-// import axios from 'axios';
 
-// axios.defaults.baseURL =
-//     'https://addressbook-wargcorp-8f592fab.koyeb.app/api/healthchecker';
-// axios.defaults.headers.get['Content-Type'] = 'application/json';
-
-// async function getUser() {
-//     try {
-//         const response = await axios.get();
-//         console.log(response);
-//     } catch (error) {
-//         console.error(error);
-//     }
-// }
 
 // getUser();
 
---
+++ src/partials/requests.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+axios.defaults.baseURL =
+    'https://addressbook-wargcorp-8f592fab.koyeb.app/api/healthchecker';
+axios.defaults.headers.get['Content-Type'] = 'application/json';
+
+async function getUser() {
+    try {
+        const response = await axios.get();
+        console.log(response);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getUser()
