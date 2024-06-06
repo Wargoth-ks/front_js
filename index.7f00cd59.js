72731353fefad802f65330ba31e7ca5c57f11da7
Wargoth-ks <121107554+Wargoth-ks@users.noreply.github.com>
2024-06-06 07:42:50 +0000

    Deploying to gh-pages from @ Wargoth-ks/front_js@ab9a663556aa9741dde62b9f2c301c2f3fb9e68e 🚀

--- index.7f00cd59.js
+++ index.7f00cd59.js
@@ -1,4 +1,4 @@
-!function(){let e=`<div class="modal-content">
+const e=`<div class="modal-content login-form">
                 <span class="close" id="loginClose">&times;</span>
                 <h2 class="modal-title">Login</h2>
                 <form class="form">
@@ -18,10 +18,12 @@
                     <input type="password" id="registerPassword" name="password" required>
                     <label for="confirmPassword">Confirm Password:</label>
                     <input type="password" id="confirmPassword" name="confirmPassword" required>
-                    <input type="checkbox" name="" id="idCheckBox">
-                    <label for="avatarImg">Choose image (optional):</label>
+                    <div class="checkbox-container">
+                        <label for="avatarImg">Choose image (optional):</label>
+                        <input type="checkbox" id="idCheckBox">
+                    </div>
                     <input disabled type="file" name="avatar" id="inputImg" accept="image/png">
                     <button type="submit">Register</button>
                 </form>
-            </div>`,a=document.querySelectorAll(".js-btns"),s=document.querySelectorAll(".modal");function t(e){e.style.display="none",e.innerHTML=""}a.forEach((a,l)=>{let o=s[l];a.addEventListener("click",()=>{switch(o.style.display="block",l){case 0:o.insertAdjacentHTML("afterbegin",e);break;case 1:o.insertAdjacentHTML("afterbegin",i),function(){let e=document.querySelector("#inputImg");document.querySelector("#idCheckBox").addEventListener("click",function(){e.disabled=!this.checked})}();break;default:console.dir(l)}(function(e){e.querySelector(".close").addEventListener("click",()=>{t(e)})})(o),document.addEventListener("keydown",e=>{"Escape"===e.code&&t(o)})})})}();
-//# sourceMappingURL=index.d6e4cb01.js.map
+            </div>`,a=document.querySelectorAll(".js-btns"),s=document.querySelectorAll(".modal");function o(e){e.style.display="none",e.innerHTML=""}a.forEach((a,t)=>{let l=s[t];a.addEventListener("click",()=>{switch(l.style.display="block",t){case 0:l.insertAdjacentHTML("afterbegin",e);break;case 1:l.insertAdjacentHTML("afterbegin",i),function(){let e=document.querySelector("#inputImg");document.querySelector("#idCheckBox").addEventListener("click",function(){e.disabled=!this.checked})}();break;default:console.dir(t)}(function(e){e.querySelector(".close").addEventListener("click",()=>{o(e)})})(l),document.addEventListener("keydown",e=>{"Escape"===e.code&&o(l)})})});
+//# sourceMappingURL=index.7f00cd59.js.map
--- index.c9c4c196.js
+++ index.c9c4c196.js
@@ -1,4 +1,4 @@
-const e=`<div class="modal-content">
+!function(){let e=`<div class="modal-content login-form">
                 <span class="close" id="loginClose">&times;</span>
                 <h2 class="modal-title">Login</h2>
                 <form class="form">
@@ -8,7 +8,7 @@ const e=`<div class="modal-content">
                     <input type="password" id="loginPassword" name="password" required>
                     <button type="submit">Login</button>
                 </form>
-            </div>`,a=`<div class="modal-content">
+            </div>`,i=`<div class="modal-content">
                 <span class="close" id="registerClose">&times;</span>
                 <h2 class="modal-title">Register</h2>
                 <form class="form">
@@ -18,10 +18,12 @@ const e=`<div class="modal-content">
                     <input type="password" id="registerPassword" name="password" required>
                     <label for="confirmPassword">Confirm Password:</label>
                     <input type="password" id="confirmPassword" name="confirmPassword" required>
-                    <input type="checkbox" name="" id="idCheckBox">
-                    <label for="avatarImg">Choose image (optional):</label>
+                    <div class="checkbox-container">
+                        <label for="avatarImg">Choose image (optional):</label>
+                        <input type="checkbox" id="idCheckBox">
+                    </div>
                     <input disabled type="file" name="avatar" id="inputImg" accept="image/png">
                     <button type="submit">Register</button>
                 </form>
-            </div>`,i=document.querySelectorAll(".js-btns"),s=document.querySelectorAll(".modal");function t(e){e.style.display="none",e.innerHTML=""}i.forEach((i,l)=>{let o=s[l];i.addEventListener("click",()=>{switch(o.style.display="block",l){case 0:o.insertAdjacentHTML("afterbegin",e);break;case 1:o.insertAdjacentHTML("afterbegin",a),function(){let e=document.querySelector("#inputImg");document.querySelector("#idCheckBox").addEventListener("click",function(){e.disabled=!this.checked})}();break;default:console.dir(l)}(function(e){e.querySelector(".close").addEventListener("click",()=>{t(e)})})(o),document.addEventListener("keydown",e=>{"Escape"===e.code&&t(o)})})});
-//# sourceMappingURL=index.695f576b.js.map
+            </div>`,a=document.querySelectorAll(".js-btns"),s=document.querySelectorAll(".modal");function t(e){e.style.display="none",e.innerHTML=""}a.forEach((a,l)=>{let o=s[l];a.addEventListener("click",()=>{switch(o.style.display="block",l){case 0:o.insertAdjacentHTML("afterbegin",e);break;case 1:o.insertAdjacentHTML("afterbegin",i),function(){let e=document.querySelector("#inputImg");document.querySelector("#idCheckBox").addEventListener("click",function(){e.disabled=!this.checked})}();break;default:console.dir(l)}(function(e){e.querySelector(".close").addEventListener("click",()=>{t(e)})})(o),document.addEventListener("keydown",e=>{"Escape"===e.code&&t(o)})})})}();
+//# sourceMappingURL=index.c9c4c196.js.map
